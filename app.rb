1f3a896edd942bcd33d2772789299692009bdbfe
Mark Shkreli <m.shkreli96@icloud.com>
2022-11-18 12:18:34 -0500

    commit tree edf6ec338b5d1b2285092da62a46bf83a9e31c47

---
+++ app.rb
@@ -0,0 +1,19 @@
+#puts is the equivalent of console.log in JS, a hashtag is used to comment out lines of code in ruby.
+#space needs to exist when using p otherwise you will get an error
+#puts places a break in between each line used, so displaying an Array with puts will render it with breaks rather than an Array.
+#pp is a better way to print out complex data, like nested arrays and hashes
+#hashes are rubys equivalent to objects in JavaScript.
+#IRB is ruby's version of REPL where we can enter lines of code in the terminal and it will run.
+#when running puts in IRB you will receive two lines of code, the value printed and a value called => nil, which is the equivilant to null meaning no value.
+
+# puts "hello, world!"
+# puts "hello, world!"
+# puts "hello, world!"
+
+# p [1, 2, 3]
+
+# pp [{ id: 1, hello: "World!" }, { id: 2, hello: "Ruby"}, {id: 3, hello: "Moon"}, { id: 4, hello: "Learner"}]
+
+puts "Hello World!"
+print "Pass this test, please."
+p [1,2,3]
